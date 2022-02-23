5f8ecee0a9826d46af16d780398004c6fac349b1
Moritz Salla <moritz.salla@hotmail.de>
2022-02-23 15:37:48 +0100

    Links

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,11 +2,10 @@ import * as d3 from 'd3';
 import { forceSimulation } from 'd3-force';
 
 import { motion } from 'framer-motion';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { data } from '../data/index';
 import '../styles.css';
-
-const contains = ({ start, end }, time) => start <= time && time < end;
+import Tile from './Tile';
 
 /**
  * Component state handled by react
@@ -18,9 +17,6 @@ const contains = ({ start, end }, time) => start <= time && time < end;
 
 const Canvas = () => {
   const ref = useRef();
-  const [nodes, setNodes] = useState(null); // how many nodes can we store in react state until it becomes unperformant?
-
-  // const normalize = d3.scaleLinear().domain([0, 100]).range([-100, 100]);
 
   useEffect(() => {
     if (!ref) {
@@ -28,27 +24,17 @@ const Canvas = () => {
       return;
     }
 
-    /**
-     * @note access data like this: simulation.nodes()
-     *
-     * index - the node’s zero-based index into nodes
-     * x - the node’s current x-position
-     * y - the node’s current y-position
-     * vx - the node’s current x-velocity
-     * vy - the node’s current y-velocity
-     */
-    const ticked = () => {
-      const nodes = simulation.nodes();
-      setNodes(nodes);
-    };
-
     const simulation = forceSimulation()
       .nodes(data.nodes)
-      .force('charge', d3.forceManyBody().strength(-30))
+      .force('charge', d3.forceManyBody().strength(-50))
       .force('center', d3.forceCenter(0, 0).strength(0.4))
       .force(
         'collide',
-        d3.forceCollide().strength(-0.2).radius(4).iterations(2)
+        d3
+          .forceCollide()
+          .strength(-0.2)
+          .radius(10) // component bounds
+          .iterations(2)
       )
       .force(
         'link',
@@ -58,18 +44,14 @@ const Canvas = () => {
           .links(data.links)
           .strength(0.008)
       )
-      .force('x', d3.forceX().strength(0.01))
-      .force('y', d3.forceY().strength(0.01))
-      .on('tick', ticked);
+      .force('x', d3.forceX().strength())
+      .force('y', d3.forceY().strength());
 
     return () => {
-      setNodes(null);
       simulation.on('end', () => console.log('simulation end'));
       simulation.stop();
     };
-  }, [nodes]);
-
-  console.log(nodes);
+  }, []);
 
   return (
     <section ref={ref} className='canvas'>
@@ -82,7 +64,21 @@ const Canvas = () => {
           window.innerHeight,
         ]}
       >
-        {nodes?.map(({ id, x, y, vx, vy }) => {
+        {/* link layer */}
+        {data?.links?.map(({ source, target }, index) => (
+          <line
+            key={`link-${index}`}
+            className='line-link'
+            stroke='grey'
+            x1={source.x}
+            y1={source.y}
+            x2={target.x}
+            y2={target.y}
+          />
+        ))}
+
+        {/* component layer */}
+        {data?.nodes?.map(({ id, x, y, vx, vy }) => {
           const boxWidth = 160;
           const boxHeight = 40;
 
@@ -92,21 +88,21 @@ const Canvas = () => {
               className='layer'
               style={{ x: x + vx, y: y + vy }}
             >
-              {/* <foreignObject
+              <foreignObject
                 width={boxWidth}
                 height={boxHeight}
                 x={-boxWidth / 2}
                 y={-boxHeight / 2}
               >
                 <Tile key={`node-${id}`} text={id} />
-              </foreignObject> */}
+              </foreignObject>
 
-              <rect
+              {/* <rect
                 className='center-dot'
                 width='5px'
                 height='5px'
                 fill='red'
-              />
+              /> */}
             </motion.g>
           );
         })}
