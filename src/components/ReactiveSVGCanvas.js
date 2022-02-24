6c8aab9046125df58c9ad2d170a5bd66b61005e2
Moritz <moritz@buildinamsterdam.com>
2022-02-24 16:53:54 +0100

    Gesture

--- src/components/ReactiveSVGCanvas.js
+++ src/components/ReactiveSVGCanvas.js
@@ -1,18 +1,17 @@
-import { motion } from 'framer-motion';
+import { useDrag } from '@use-gesture/react';
+import { motion, useMotionValue, useSpring, useTransform } from 'framer-motion';
 import { useEffect, useRef } from 'react';
 import { data as dataset } from '../data/index';
 import '../styles.css';
 import { graphProgram } from './graphProgram';
 import Tile from './Tile';
 
-const ZOOM_LEVEL = 1; // you'll probably want to use 1 here
-
 /**
- * Component state handled by react
- * Positioning/animation handled OUTSIDE OF REACT by framer
+ * Component state handled by react.
+ * Any repeated positioning/animation should be handled outside of react state.
  * @example https://github.com/d3/d3-force
  * @example https://observablehq.com/@d3/temporal-force-directed-graph?collection=@d3/d3-force
- * @note D3 is implicit - it mutates original data
+ * @note D3 works implicitly
  */
 const ReactiveSVGCanvas = () => {
   // storing mutated data in ref to persist between renders
@@ -21,57 +20,84 @@ const ReactiveSVGCanvas = () => {
   // cleanup
   useEffect(() => graphRef.current?.cleanup(), [graphRef]);
 
+  const x = useSpring(0, { stiffness: 300, damping: 30 });
+  const y = useSpring(0, { stiffness: 300, damping: 30 });
+  const normX = useTransform(x, [0, window.innerWidth * 2], [-100, 100]);
+  const normY = useTransform(y, [0, window.innerHeight * 2], [-100, 100]);
+
+  const bind = useDrag(({ xy }) => {
+    x.set(xy[0]);
+    y.set(xy[1]);
+  });
+
+  // // for 1 / 1 zoom level
+  // const viewBox = [
+  //   -window.innerWidht * .5,
+  //   -window.innerHeight * .5,
+  //   window.innerWidth * 2,
+  //   window.innerHeight * 2,
+  // ];
+
   const viewBox = [
-    (-window.innerWidth / 2) * ZOOM_LEVEL,
-    (-window.innerHeight / 2) * ZOOM_LEVEL,
-    window.innerWidth * ZOOM_LEVEL,
-    window.innerHeight * ZOOM_LEVEL,
+    -window.innerWidht,
+    -window.innerHeight,
+    window.innerWidth * 2,
+    window.innerHeight * 2,
   ];
 
   return (
-    <section className='canvas'>
-      <svg className='layer-container' viewBox={viewBox}>
-        {/* link layer */}
-        {graphRef.current?.links?.map(({ source, target }, index) => (
-          <motion.line
-            key={`link-${index}`}
-            className='line-link'
-            stroke='grey'
-            animate={{
-              x1: source.x,
-              y1: source.y,
-              x2: target.x,
-              y2: target.y,
-            }}
-          />
-        ))}
+    <section className='canvas' {...bind()}>
+      <motion.div
+        className='canvas-draggable-inner'
+        style={{ x: normX, y: normY }}
+      >
+        <motion.svg className='svg' viewBox={viewBox}>
+          {/* link layer */}
+          {graphRef.current?.links?.map(({ source, target }, index) => (
+            <motion.line
+              key={`link-${index}`}
+              className='line-link'
+              stroke='grey'
+              animate={{
+                x1: source.x,
+                y1: source.y,
+                x2: target.x,
+                y2: target.y,
+              }}
+            />
+          ))}
 
-        {/* component layer */}
-        {graphRef.current?.nodes?.map(({ id, x, y }) => {
-          const boxWidth = 200;
-          const boxHeight = 200;
+          {/* component layer */}
+          {graphRef.current?.nodes?.map(({ id, x, y }) => {
+            const boxWidth = 200;
+            const boxHeight = 200;
 
-          return (
-            <motion.g key={`layer-${id}`} className='layer' animate={{ x, y }}>
-              <foreignObject
-                width={boxWidth}
-                height={boxHeight}
-                x={-boxWidth / 2}
-                y={-boxHeight / 2}
+            return (
+              <motion.g
+                key={`layer-${id}`}
+                className='layer'
+                animate={{ x, y }}
               >
-                <Tile key={`node-${id}`} text={id} />
-              </foreignObject>
+                <foreignObject
+                  width={boxWidth}
+                  height={boxHeight}
+                  x={-boxWidth / 2}
+                  y={-boxHeight / 2}
+                >
+                  <Tile key={`node-${id}`} text={id} />
+                </foreignObject>
 
-              {/* <rect
+                {/* <rect
                 className='center-dot'
                 width='5px'
                 height='5px'
                 fill='red'
               /> */}
-            </motion.g>
-          );
-        })}
-      </svg>
+              </motion.g>
+            );
+          })}
+        </motion.svg>
+      </motion.div>
     </section>
   );
 };
