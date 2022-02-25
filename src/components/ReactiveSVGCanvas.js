5290ee2c1db73ab5a80e5c3fdd40eb3a362a0931
Moritz <moritz@buildinamsterdam.com>
2022-02-25 16:03:48 +0100

    Drag

--- src/components/ReactiveSVGCanvas.js
+++ src/components/ReactiveSVGCanvas.js
@@ -1,19 +1,17 @@
 import * as d3 from 'd3';
 import {
+  forceCollide,
   forceLink,
   forceManyBody,
   forceSimulation,
-  forceX,
-  forceY,
 } from 'd3-force';
 import { motion } from 'framer-motion';
 import { useEffect } from 'react';
 import { data } from '../data/';
 import '../styles.css';
-import { Dot } from '../utils/helpers';
 import Tile from './Tile';
 
-const ZOOM_LEVEL = 2;
+const ZOOM_LEVEL = 1;
 
 /**
  * D3 simulation rendering react components.
@@ -23,19 +21,49 @@ const ZOOM_LEVEL = 2;
  */
 const ReactiveSVGCanvas = () => {
   useEffect(() => {
+    const drag = (simulation) => {
+      function dragstarted(event, d) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+      }
+
+      function dragged(event, d) {
+        d.fx = event.x;
+        d.fy = event.y;
+      }
+
+      function dragended(event, d) {
+        if (!event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+      }
+
+      return d3
+        .drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended);
+    };
+
     const ticked = () => {
       d3.selectAll('g')
         .data(data.nodes)
         .attr('transform', ({ x, y }) => {
           return `translate(${x}, ${y})`;
-        });
+        })
+        .call(drag(simulation));
 
       d3.selectAll('line')
         .data(data.links)
         .attr('x1', ({ source }) => source.x)
         .attr('y1', ({ source }) => source.y)
         .attr('x2', ({ target }) => target.x)
-        .attr('y2', ({ target }) => target.y);
+        .attr('y2', ({ target }) => target.y)
+        .style('stroke', function ({ value }) {
+          return value < 0.5 ? 'red' : 'white';
+        })
+        .call(drag(simulation));
     };
 
     const simulation = forceSimulation()
@@ -47,9 +75,8 @@ const ReactiveSVGCanvas = () => {
           .strength(0.01)
       )
       .force('charge', forceManyBody().strength(-300))
-      .force('center', forceManyBody().strength(-50))
-      .force('x', forceX().strength())
-      .force('y', forceY().strength())
+      .force('collide', forceCollide().radius(100).iterations(1))
+      .force('center', forceManyBody().strength(200))
       .on('tick', ticked);
 
     return () => simulation.stop();
@@ -73,8 +100,8 @@ const ReactiveSVGCanvas = () => {
 
           {/* component layer */}
           {data?.nodes?.map(({ id }, index) => {
-            const boxWidth = 200;
-            const boxHeight = 200;
+            const boxWidth = 100;
+            const boxHeight = 100;
 
             return (
               <motion.g key={`layer-${index}`} className='layer'>
@@ -86,8 +113,6 @@ const ReactiveSVGCanvas = () => {
                 >
                   <Tile key={`node-${id}`} />
                 </foreignObject>
-
-                <Dot />
               </motion.g>
             );
           })}
